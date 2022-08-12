fd1fda1b91607d30e4d2cbc0968628bc2a145f89
SaifK94 <Saif.K94@outlook.com>
2022-08-12 12:58:07 +1000

    Print Name
    this code will print my name "Saif Khan" 20 times horizontally then 20 times vertically

---
+++ tasks/task_2.py
@@ -0,0 +1,12 @@
+"""
+Task 2: strings and ints
+
+Get familiar with the building blocks of python by printing my name 20 times
+"""
+firstName = "Saif "
+lastName = "Khan"
+print ((firstName+lastName)
+       *20)
+
+for i in range(20):
+    print (firstName+lastName)
