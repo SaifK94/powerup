16505b8f27fee26a5ce54398dca9712b69774cc3
dscott3d <46595931+dscott3d@users.noreply.github.com>
2022-08-12 14:07:24 +1000

    Different approach

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -5,7 +5,9 @@ Get familiar with the building blocks of python by printing my name 20 times
 """
 firstName = "Saif "
 lastName = "Khan"
-print ((firstName+lastName)*20)
 
-for i in range(20):
-    print (firstName+lastName)
+new_msg = (firstName+lastName+"\n")*20
+print (new_msg)
+
+# for i in range(1, 20+1):
+#     print(i, firstName+lastName)
