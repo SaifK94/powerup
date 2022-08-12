47c4d00b62cd976f5e39dce5db0c77ee9745dd5e
SaifK94 <Saif.K94@outlook.com>
2022-08-12 13:00:02 +1000

    adjustement to code
    removed an unnecessary line break

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -5,8 +5,7 @@ Get familiar with the building blocks of python by printing my name 20 times
 """
 firstName = "Saif "
 lastName = "Khan"
-print ((firstName+lastName)
-       *20)
+print ((firstName+lastName)*20)
 
 for i in range(20):
     print (firstName+lastName)
