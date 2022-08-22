b1c6b43c39ef4d17fe985dc16324f977d3722ea6
SaifK94 <Saif.K94@outlook.com>
2022-08-22 10:19:03 +1000

    Listing Tim Tams
    I made a list of 11 Tim Tams and used a slice command to remove 2 Tim Tams from the top of the list until only 1 was left.

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,9 +1,44 @@
 """
 Task 3: Tim Tams
 
-Daniel likes tim tams ... there's a packet of 11, he eats two at a time until there's only one left.
+Daniel likes tim tams ... there's a packet of 11, he eats two at a time
+until there's only one left.
 
 Please print this out.
 
 Some variable: name, number_of_tim_tams
 """
+TimTams = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print('Daniel has')
+print(TimTams)
+print('Tim Tams')
+print('CHOMP')
+print('Daniel eats 2 Tim Tams.')
+TimTams[:2] = []
+print('Daniel now has')
+print(TimTams)
+print('Tim Tams')
+print('CHOMP')
+print('Daniel eats 2 Tim Tams.')
+TimTams[:2] = []
+print('Daniel now has')
+print(TimTams)
+print('Tim Tams')
+print('CHOMP')
+print('Daniel eats 2 Tim Tams.')
+TimTams[:2] = []
+print('Daniel now has')
+print(TimTams)
+print('Tim Tams')
+print('CHOMP')
+print('Daniel eats 2 Tim Tams.')
+TimTams[:2] = []
+print('Daniel now has')
+print(TimTams)
+print('Tim Tams')
+print('CHOMP')
+print('Daniel eats 2 Tim Tams.')
+TimTams[:2] = []
+print('Daniel now has')
+print(TimTams)
+print('Tim Tam')
